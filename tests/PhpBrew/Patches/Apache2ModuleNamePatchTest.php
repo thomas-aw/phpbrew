4ef1cc533cc39138ab02bc416772e2cb7a0a422a
c9s <yoanlin93@gmail.com>
2016-04-27 14:56:17 +0800

    Added new apxs2 patch test

---
+++ tests/PhpBrew/Patches/Apache2ModuleNamePatchTest.php
@@ -0,0 +1,46 @@
+<?php
+use CLIFramework\Logger;
+use PhpBrew\Build;
+use PhpBrew\Patches\IntlWith64bitPatch;
+use PhpBrew\Patches\Apache2ModuleNamePatch;
+use PhpBrew\Utils;
+use PhpBrew\Testing\PatchTestCase;
+
+/**
+ * @small
+ */
+class Apache2ModuleNamePatchTest extends PatchTestCase
+{
+    public function testPatch()
+    {
+        $logger = new Logger();
+        $logger->setQuiet();
+
+        $version = '5.5.17';
+        $sourceFixtureDirectory = getenv('PHPBREW_FIXTURES_PHP_DIR') . DIRECTORY_SEPARATOR . $version;
+        $sourceExpectedDirectory = getenv('PHPBREW_EXPECTED_PHP_DIR') . DIRECTORY_SEPARATOR . $version;
+        $sourceDirectory = getenv('PHPBREW_BUILD_PHP_DIR');
+
+        if (!is_dir($sourceDirectory)) {
+            return $this->markTestSkipped("$sourceDirectory does not exist.");
+        }
+
+        // Copy the source Makefile to the Makefile
+        copy($sourceFixtureDirectory . '/Makefile.global', $sourceDirectory . '/Makefile.global');
+        copy($sourceFixtureDirectory . '/configure', $sourceDirectory . '/configure');
+
+        $build = new Build($version);
+        $build->setSourceDirectory($sourceDirectory);
+        $build->enableVariant('apxs2');
+        $this->assertTrue($build->hasVariant('apxs2'), 'apxs2 enabled');
+
+        $patch = new Apache2ModuleNamePatch;
+        $matched = $patch->match($build, $logger);
+        $this->assertTrue($matched, 'patch matched');
+        $patchedCount = $patch->apply($build, $logger);
+        $this->assertEquals(107, $patchedCount);
+
+        $this->assertFileEquals($sourceExpectedDirectory. '/Makefile.global', $sourceDirectory . '/Makefile.global');
+        $this->assertFileEquals($sourceExpectedDirectory. '/configure', $sourceDirectory . '/configure');
+    }
+}
